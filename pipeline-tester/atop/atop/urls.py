0a3df9d76a72ed72283d1588bfeac95d3afbc119
Jérôme Charrière <je.charriere@gmail.com>
2018-05-01 04:33:06 -0400

    Added URL patterns injections if Django is in test mode
    This is done so that we can setup CARMIN/URL simulators bound to specific URLs, but only if we are currently testing.

--- pipeline-tester/atop/atop/urls.py
+++ pipeline-tester/atop/atop/urls.py
@@ -16,7 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from . import views
+
+from atop import views
+from atop.tests import simulators
+from atop.settings import TESTING
+import sys
+
+if (TESTING):
+    test_urls =  [
+        path('carmin_simulation/<carmin_server>/<method>/', simulators.carmin_simulation_dispatcher, name="carmin_simulation_dispatcher"), 
+        path('carmin_simulation/<carmin_server>/pipelines/<pipeline_id>/boutiquesdescriptor', simulators.carmin_simulation_dispatcher, name="carmin_simulation_dispatcher"),
+        path('urldesc_simulation/<url>/', simulators.urldesc_simulation_dispatcher, name="urldesc_simulation_dispatcher"),
+]
+
+else:
+    test_urls = []
 
 urlpatterns = [
     path('', views.home),
@@ -28,4 +42,9 @@ urlpatterns = [
     path('run_tests/', views.run_tests, name="run_tests"),
     path('validate_descriptor/', views.validate, name="validate"),
     path('logout/', views.log_out, name="log_out"),
-]
+    #path('carmin_simulation/<carmin_server>/<method>/', simulators.carmin_simulation_dispatcher, name="carmin_simulation_dispatcher"),
+    #path('carmin_simulation/<carmin_server>/pipelines/<pipeline_id>/boutiquesdescriptor', simulators.carmin_simulation_dispatcher, name="carmin_simulation_dispatcher"),
+
+] + test_urls
+
+
