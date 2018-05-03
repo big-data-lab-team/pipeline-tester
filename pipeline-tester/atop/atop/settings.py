bc3dcbe1767c039352ae4baedddbb72dc86e3f32
Jérôme Charrière <je.charriere@gmail.com>
2018-05-03 05:40:12 -0400

    Added constant that allow the presence of static files

--- pipeline-tester/atop/atop/settings.py
+++ pipeline-tester/atop/atop/settings.py
@@ -124,7 +124,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
 #DJANGO_TABLES2_TEMPLATE = 'django_tables2/bootstrap-responsive.html'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
+
 
 ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
 
